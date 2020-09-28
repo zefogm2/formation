1e5ba4cfc3c4219ef9214813a980c9941356922d
zefogm2 <remybaron73@gmail.com>
2020-09-28 15:16:27 +0200

    Utilisation de la transclusion (ng-content) , Ui est maintenant réutilisable

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
+  ,exports: [NavComponent, HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ui.component.scss']
 })
 export class UiComponent implements OnInit {
-  public title: string;
+
   public menuIsOpen:boolean;
   constructor() { }
 
   ngOnInit(): void {
     this.menuIsOpen=false;
-    this.title="Angular c'est trop de la balle !!!!";
+
   }
   public toggleMenu() {
         this.menuIsOpen=!this.menuIsOpen;
